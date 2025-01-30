c75a6360fe2e6ab284223143af53468f8318b7b9
Sahassawat Posungnern <maxinon24@gmail.com>
2025-01-30 12:50:22 +0700

    to deploy

--- appraisal_home.py
+++ appraisal_home.py
@@ -26,9 +26,6 @@ def is_within_radius(center, point, radius):
 df['is_within_radius'] = df.apply(lambda row: is_within_radius(center_point, (row['latitude'], row['longitude']), radius_km), axis=1)
 filtered_df = df[df['is_within_radius']]
 
-# Check if filtered_df contains data
-print(filtered_df.head())
-
 # Create the Map
 map_center = folium.Map(location=center_point, zoom_start=13)
 
@@ -37,9 +34,11 @@ folium.Marker(center_point, popup='Center Point', icon=folium.Icon(color='red'))
 
 # Add the Filtered Points to the Map
 for _, row in filtered_df.iterrows():
-    popup_content = f"Field 1: {row['name_th']}<br>Field 2: {row['developer_name_th']}<br>Field 3: {row['price_min']}<br>Field 4: {row['url_project']}"
+    popup_content = f"Name: {row['name_th']}<br>Developer: {row['developer_name_th']}<br>Price: {row['price_min']}<br>Link: {row['url_project']}"
     folium.CircleMarker(location=(row['latitude'], row['longitude']), radius=2, color='blue', fill=True, fill_opacity=0.6, popup=popup_content).add_to(map_center)
+
+# Add Click Event to Show Popup Information
+map_center.add_child(folium.ClickForMarker(popup='Click Point'))
 
 # Save the Map
 map_center.save('index.html')
-print("Map saved as 'index.html'.")
